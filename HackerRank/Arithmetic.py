e86bf26e13c1b24a670bed5e9060c9b49d3799e4
Kenny Claka <kennyigbechi@gmail.com>
2018-12-28 23:25:47 -0800

    Solved a new challenge

---
+++ HackerRank/Arithmetic.py
@@ -0,0 +1,15 @@
+'''
+Created on Dec 28, 2018
+
+@author: K3NN!
+
+url : https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
+'''
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    print(a + b)
+    print(a - b)
+    print(a * b)
