0e54592d457aae99b7ba6213af94babecc9fd1fb
Kenny Claka <kennyigbechi@gmail.com>
2018-12-28 22:54:08 -0800

    Python If-Else

---
+++ HackerRank/If-Else.py
@@ -0,0 +1,16 @@
+'''
+Created on Dec 28, 2018
+
+@author: K3NN!
+'''
+
+
+N = int(input())
+
+if N % 2 == 0:
+    print("Not Weird")
+else:
+    print("Weird")
+
+if __name__ == '__main__':
+    pass
